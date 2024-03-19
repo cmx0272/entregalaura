c905d022e58ab558ab0b6425a2d6c19058733d75
laura <laura@LauraLenovo.home>
2024-03-19 19:30:48 +0100

    entrega proyecto 1

---
+++ PROYECTO_LAURA/src/fp/tipos/test/TestFechas.java
@@ -0,0 +1,40 @@
+package fp.tipos.test;
+
+import fp.tipos.Fechas;
+
+public class TestFechas {
+
+    public static void main(String[] args) {
+        // METODO OF(año, mes, dia)
+        Fechas fecha1 = Fechas.of(2023, 10, 6);
+        System.out.println("Fecha 1: " + fecha1);
+
+        // METODO PARSE(cadenaFecha)
+        Fechas fecha2 = Fechas.parse("2023-09-20");
+        System.out.println("Fecha 2: " + fecha2);
+
+        // NOMBRE MES
+        String nombreMes = fecha1.nombreMes();
+        System.out.println("Nombre del mes de la Fecha 1: " + nombreMes);
+
+        // DIA SEMANA
+        String diaSemana = fecha1.diaSemana();
+        System.out.println("Dia de la semana de la Fecha 1: " + diaSemana);
+
+        // SUMA DIAS
+        Fechas fecha3 = fecha1.sumarDias(5);
+        System.out.println("Fecha 3 (sumando 5 dias a la Fecha 1): " + fecha3);
+
+        // RESTA DIAS
+        Fechas fecha4 = fecha1.restarDias(3);
+        System.out.println("Fecha 4 (restando 3 dias a la Fecha 1): " + fecha4);
+
+        // DIFERENCIA DIAS
+        int diferencia = fecha1.diferenciaEnDias(fecha2);
+        System.out.println("Diferencia en dias entre Fecha 1 y Fecha 2: " + diferencia);
+
+        // AÑO BISIESTO
+        boolean esBisiesto = Fechas.esAñoBisiesto(2024);
+        System.out.println("2024 es bisiesto: " + esBisiesto);
+    }
+}
